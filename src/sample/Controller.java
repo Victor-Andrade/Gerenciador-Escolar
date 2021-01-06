2f63ed76c00c54db113340996276b0dd9fb51fbf
Pedro Vinícius de Melo Silva <PedroViniciusMelo@users.noreply.github.com>
2021-01-06 20:59:22 -0300

    Adição de um botão para testes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+
 public class Controller {
+    public Text txt;
+    public Button btn;
+
+    public void btnClick() {
+        txt.setText("Deu certo!");
+    }
 }
